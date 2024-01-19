0e0ea8e1fa048d70d2d645ed82a11979f1fe1a0a
hyun9758 <jj020305@likelion.org>
2024-01-19 11:17:45 +0900

    [실습] 상태 변수를 설정해 모드 전환에 따른 React 리렌더링

--- src/lecture/11-typescript-jsx.js
+++ src/lecture/11-typescript-jsx.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 const createApp = () => {
-    return (<div id="app">
-            <h1>안녕!<br />리액트</h1>
-            <p>리액트는 오픈 소스 자바스크립트 라이브러리입니다.</p>
-        </div>);
+    return (React.createElement("div", { id: "app" },
+        React.createElement("h1", null,
+            "\uC548\uB155!",
+            React.createElement("br", null),
+            "\uB9AC\uC561\uD2B8"),
+        React.createElement("p", null, "\uB9AC\uC561\uD2B8\uB294 \uC624\uD508 \uC18C\uC2A4 \uC790\uBC14\uC2A4\uD06C\uB9BD\uD2B8 \uB77C\uC774\uBE0C\uB7EC\uB9AC\uC785\uB2C8\uB2E4.")));
 };
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
---
+++ src/lecture/12-web-component.js
@@ -0,0 +1,4 @@
+// 웹 컴포넌트 API (웹 표준)
+// globalThis.customElements.define('custom-element', CustomComponent)
+// <custom-element></custom-element>
+
