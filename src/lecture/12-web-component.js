2409d7010585b449742fc087d307b857e7a5f9ac
hyun9758 <jj020305@likelion.org>
2024-01-19 21:50:31 +0900

    놓친 부분 백업하기

--- src/lecture/12-web-component.js
+++ src/lecture/12-web-component.js
@@ -1,4 +1,21 @@
 // 웹 컴포넌트 API (웹 표준)
-// globalThis.customElements.define('custom-element', CustomComponent)
-// <custom-element></custom-element>
+// 슬롯(Slot)
 
+if ('customElements' in globalThis) {
+    customElements.define('euid-stack', class extends HTMLElement {
+        constructor() {
+            super();
+            this.attachShadow({ mode: 'open' });
+            this.shadowRoot.innerHTML = `
+                <div class="euid-stack">
+                    <h2>
+                        <slot name="headline"></slot>
+                    </h2>
+                    <p>
+                        <slot name="description"></slot>
+                    </p>
+                </div>
+            `;
+        }
+    })
+}
