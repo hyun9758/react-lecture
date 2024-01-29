c5d671b81677d175fa530e1de1cc1c87b9f39680
hyun9758 <jj020305@likelion.org>
2024-01-29 15:02:02 +0900

    머지 전 커밋

---
+++ src/components/Graph/Graph.jsx
@@ -0,0 +1,7 @@
+import classes from './Graph.module.css';
+
+function Graph() {
+  return <div className={classes.Graph}>Graph</div>;
+}
+
+export default Graph;
---
+++ src/components/Graph/Graph.stories.js
@@ -0,0 +1,13 @@
+import Graph from "./Graph";
+
+const metaConfig = {
+    tutle : 'components/Graph',
+    component: Graph,
+    tags:['autodocs'],
+}
+
+export default metaConfig;
+
+export const Base = {
+
+}
