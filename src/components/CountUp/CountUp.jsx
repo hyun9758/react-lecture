eba5db2f7fb1a4ca3f152b6b0fd1cb2409096bed
hyun9758 <jj020305@likelion.org>
2024-01-30 09:27:09 +0900

    머지 전 커밋

--- src/components/CountUp/CountUp.jsx
+++ src/components/CountUp/CountUp.jsx
@@ -1,6 +1,13 @@
 import classes from './CountUp.module.css';
 
 function CountUp({ count = 0 }) {
+  const handleCountUp = (e) => {
+    // console.log('clicked button');
+    // 변이(mutation) : 변경(update) 시도
+    // 과연??
+    count = count + 1;
+  };
+
   return (
     <div className={classes.CountUp}>
       <output>{count}</output>
